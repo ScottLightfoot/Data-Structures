1929185a48a96f52dc6958cc9d0d6f08472bf48c
ScottLightfoot <gslightfoot@gmail.com>
2020-08-10 15:57:08 -0500

    singly complete, tests passing, needs cleanup

--- queue/queue.py
+++ queue/queue.py
@@ -16,13 +16,33 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.vals = []
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.vals.insert(0, value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if len(self.vals) > 0:
+            self.size -= 1
+            return self.vals.pop()
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.append(value)
+        self.size += 1
+
+    def pop(self):
+        if len(self.storage) > 0:
+            self.size -= 1
+            return self.storage.pop()
--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,13 +1,63 @@
-class Stack:
+class Node:
+    def __init__(self, val_in, next_node=None):
+        self.value = val_in
+        self.next_node = next_node
+
+    def get_val(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
     def __init__(self):
-        self.storage = []
+        self.head = None
+        self.tail = None
 
-    def __len__(self):
-        return len(self.storage)
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+    
+    def remove_head(self):
+        if self.head is not None:
+            val_out = self.head.get_val()
+            self.head = self.head.get_next()
+            if self.head == None:
+                self.tail = None
+            return val_out
+        else:
+            return None
+    
+    def contains(self, val):
+        if self.head is not None:
+            curr = self.head
+            while curr.get_next() is not None:
+                if curr.get_val() == val:
+                    return True
+                else:
+                    curr = curr.get_next()
+            if curr.get_val() == val:
+                return True
+            return False
+        else:
+            return None
+    
+    def get_max(self):
+        if self.head == None:
+            return None
+        else:
+            max_val = 0
+            curr = self.head
+            while curr.get_next() is not None:
+                max_val = max(max_val, curr.get_val())
+                curr = curr.get_next()
+        return max(max_val, curr.get_val())
 
-    def push(self, value):
-        self.storage.append(value)
-
-    def pop(self):
-        if len(self.storage) > 0:
-            return self.storage.pop()
